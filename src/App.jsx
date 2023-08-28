cdbc72a87f62fcbfe7e598dd925fbfc4412d1a75
Sangam Rai <122705359+sangamrai07@users.noreply.github.com>
2023-08-28 13:04:37 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DemoForm from './components/demoForm'
+// import Form from './components/form'
+
+function App() {
+  return (
+    <div>
+      <DemoForm/>
+    </div>
+  )
+}
+
+export default App
+
